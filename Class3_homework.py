f6fefeb1846b5eed85b59db342bc4f6365b5cd6a
Debbie Yu <debbiey@gmail.com>
2015-06-14 12:44:51 -0400

    changed q3 and 4 to list comp

--- Class3_homework.py
+++ Class3_homework.py
@@ -31,14 +31,10 @@ PART 3: Calculate the average price of an order.
 Hint: Examine the data to see if the 'quantity' column is relevant to this calculation.
 Hint: Think carefully about the simplest way to do this!
 '''
-#loop through the rows, sum the prices associated with the same order ID, 
+#loop through the rows 
 #then add those total values per order and divide by 1834
-order_list = [row[0] for row in order_data]
-order_list_totals = []
-for order in order_data:
-	if order_data[0] == order_list[0]:
-		order_data[4]
-
+order_list_totals = [float(order[4][1:]) for order in order_data]
+average_price = round(sum(order_list_totals)/(len(set(order_num))), 2)
 
 '''
 INTERMEDIATE LEVEL
@@ -47,16 +43,9 @@ Note: Just look for 'Canned Soda' and 'Canned Soft Drink', and ignore other drin
 '''
 #go through each row. If it has 'Canned Soda' or 'Canned Soft Drink', in 2nd column, pull
 #3rd column value into empty list.  If that value is in the list, don't add. 
-
-uniq_drinks=[]
-for row in order_data:
-    if "Canned Soda" == row[2]:
-        uniq_drinks.append(row[3])
-    elif "Canned Soft Drink" == row[2]:
-        uniq_drinks.append(row[3])
-          
+         
+uniq_drinks = [row[3] for row in order_data if "Canned" in row[2]]
 print set(uniq_drinks)		
-
 
 '''
 ADVANCED LEVEL
