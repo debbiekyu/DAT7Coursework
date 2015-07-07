2959368eef30cce2d8f262a9088076f47456b051
Debbie Yu <debbiey@gmail.com>
2015-07-07 09:34:25 -0400

    testing dataframe

--- Project/NeighborhoodClusterScraper.py
+++ Project/NeighborhoodClusterScraper.py
@@ -14,9 +14,35 @@ import requests
 r = requests.get('http://neighborhoodinfodc.org/nclusters/Nbr_prof_clus2.html')
 b = BeautifulSoup(r.text)
 
-cluster_data = {}
-cluster_data['clusters'] = range(1,40)
-cluster_data['pop2010'] 
+clusters=range(1,40)
+Pop2010=[]
+Bl2010=[]
+Wh2010=[]
+Hs2010=[]
+As2010=[]
+
+def get_cluster_data(cluster):
+    r = requests.get('http://neighborhoodinfodc.org/nclusters/Nbr_prof_clus' + str(cluster) + '.html')
+    b = BeautifulSoup(r.text)
+    Pop2010.append(b('table')[2].find_all('tr')[6].find_all('td')[1].text)
+    Bl2010.append(b('table')[2].find_all('tr')[29].find_all('td')[1].text)
+    Wh2010.append(b('table')[2].find_all('tr')[32].find_all('td')[1].text)
+    Hs2010.append(b('table')[2].find_all('tr')[35].find_all('td')[1].text)
+    As2010.append(b('table')[2].find_all('tr')[38].find_all('td')[1].text)
+    
+[get_cluster_data(num) for num in clusters]  
+    
+    
+pop_dict=dict(zip(clusters,Pop2010))
+Bl_dict=dict(zip(clusters,Bl2010))
+Data=[pop_dict, Bl_dict]
+import pandas as pd
+
+data2=pd.DataFrame(Data).T
+data2.columns = ['Pop_2010','Bl_2010']
+pd.DataFrame(Data,index='clusters', columns=['Pop_2010','Bl_2010'])
+
+#look up defaultdict!
 
 Pop2010 = b('table')[2].find_all('tr')[6].find_all('td')[1].text
 #the above prints the table for population in 2010
@@ -30,9 +56,3 @@ As2010 = b('table')[2].find_all('tr')[38].find_all('td')[1].text
 #the above prints the data for % Asian/PI non-Hispanic 2010
 
 #need to append b,c,d
-
-def get_cluster_data(num_cluster):
-    r = requests.get('http://neighborhoodinfodc.org/nclusters/Nbr_prof_clus' + cluster + '.html')
-    b = BeautifulSoup(r.text)
-    Clustercluster = {}
-    Cluster['Pop2010'] = 
